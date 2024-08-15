78e6e01b70fa9cf4a3ad6d01cf8e47a920e5b56c
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-08-15 18:06:26 +0200

    feat(routes): add levels route with POST request to create level in MySQL
    - Added a new route for levels in the routes directory
    - Implemented a POST request handler to create a new level in the MySQL database
    - Integrated the new level creation logic with the Level model and MySQL
    - Ensured proper validation and error handling in the route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import usersRoutes from './users.routes';
+import levelsRoutes from './levels.routes';
 
 const router = express.Router({ mergeParams: true });
 
 router.use('/users', usersRoutes);
+router.use('/levels', levelsRoutes);
 
 export default router;
---
+++ src/routes/levels.routes.ts
@@ -0,0 +1,50 @@
+import express, { type Request, type Response } from 'express';
+import LevelModel from '../models/level';
+import { type ILevel } from '../interfaces';
+import { handleError } from '../utils/handleError';
+import { type Model } from 'sequelize';
+
+type LevelInterface = Model<ILevel>
+
+const levelsRouter = express.Router({ mergeParams: true });
+
+levelsRouter.get('/', async (req: Request, res: Response) => {
+    try {
+      const LevelsList = await LevelModel.findAll();
+      console.log('levels', LevelsList);
+      
+      res.status(200).send(LevelsList);
+    } catch (error) {
+      console.log('error', error);
+      return handleError(error);
+    }
+});
+
+levelsRouter.post('/', async (req: Request, res: Response) => {
+  const newLevel: ILevel = req.body as ILevel
+  const id: string = newLevel.external_id
+  try {
+    const existingLevel: LevelInterface | null = await LevelModel.findOne({
+      where: { external_id: id },
+    });
+    if (existingLevel == null) {
+      const createdLevel: LevelInterface = await LevelModel.create({...newLevel});
+      return res.status(201).send({
+        response: {
+          message: 'LEVEL_CREATED_SUCCESSFULLY',
+          code: 201,
+          data: createdLevel,
+        },
+      });
+    };
+    return res.status(200).send({
+      response: {
+        message: 'LEVEL_ALREADY_EXISTS',
+        code: 200,
+      },
+    });
+  } catch (error) {
+    return handleError(error);
+  }
+});
+export default levelsRouter;
