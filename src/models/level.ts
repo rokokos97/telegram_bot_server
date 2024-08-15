19f4bddde3fd18c77e3776df9766732525b4f914
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-08-15 15:57:15 +0200

    feat(models): create Level model for MySQL
    - Added Level model to represent levels in the MySQL database
    - Defined schema and data types for the Level entity
    - Integrated Level model with Sequelize for database interactions

---
+++ src/models/level.ts
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database';
+
+const Level = sequelize.define(
+  'Level',
+  {
+    id: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    numberOfCodeLines: {
+      type: DataTypes.STRING,
+    },
+    imgUrl: {
+      type: DataTypes.STRING,
+    },
+    xlevel: {
+      type: DataTypes.STRING,
+    },
+    maxLines: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: true,
+    tableName: 'Levels',
+  },
+);
+
+export default Level;
