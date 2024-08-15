bcd92b3c12a9847bd96204411c44b58eb894225d
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-08-15 21:09:36 +0200

    feat(routes): add DELETE method in levels.routes.ts to remove level
    - Implemented DELETE method in levels.routes.ts to handle level deletion
    - Integrated with the Level model to remove entries from the MySQL database
    - Added error handling and validation to ensure safe deletion of levels

--- src/routes/levels.routes.ts
+++ src/routes/levels.routes.ts
@@ -4,31 +4,33 @@ import { type ILevel } from '../interfaces';
 import { handleError } from '../utils/handleError';
 import { type Model } from 'sequelize';
 
-type LevelInterface = Model<ILevel>
+type LevelInterface = Model<ILevel>;
 
 const levelsRouter = express.Router({ mergeParams: true });
 
 levelsRouter.get('/', async (req: Request, res: Response) => {
-    try {
-      const LevelsList = await LevelModel.findAll();
-      console.log('levels', LevelsList);
-      
-      res.status(200).send(LevelsList);
-    } catch (error) {
-      console.log('error', error);
-      return handleError(error);
-    }
+  try {
+    const LevelsList = await LevelModel.findAll();
+    console.log('levels', LevelsList);
+
+    res.status(200).send(LevelsList);
+  } catch (error) {
+    console.log('error', error);
+    return handleError(error);
+  }
 });
 
 levelsRouter.post('/', async (req: Request, res: Response) => {
-  const newLevel: ILevel = req.body as ILevel
-  const id: string = newLevel.external_id
+  const newLevel: ILevel = req.body as ILevel;
+  const id: string = newLevel.external_id;
   try {
     const existingLevel: LevelInterface | null = await LevelModel.findOne({
       where: { external_id: id },
     });
     if (existingLevel == null) {
-      const createdLevel: LevelInterface = await LevelModel.create({...newLevel});
+      const createdLevel: LevelInterface = await LevelModel.create({
+        ...newLevel,
+      });
       return res.status(201).send({
         response: {
           message: 'LEVEL_CREATED_SUCCESSFULLY',
@@ -36,10 +38,35 @@ levelsRouter.post('/', async (req: Request, res: Response) => {
           data: createdLevel,
         },
       });
-    };
+    }
     return res.status(200).send({
       response: {
         message: 'LEVEL_ALREADY_EXISTS',
+        code: 200,
+      },
+    });
+  } catch (error) {
+    return handleError(error);
+  }
+});
+levelsRouter.delete('/:id', async (req: Request, res: Response) => {
+  const levelId: string = req.params.id;
+  try {
+    const existingLevel: LevelInterface | null = await LevelModel.findOne({
+      where: { external_id: levelId },
+    });
+    if (existingLevel == null) {
+      return res.status(404).send({
+        response: {
+          message: 'LEVEL_NOT_FOUND',
+          code: 404,
+        },
+      });
+    }
+    await existingLevel.destroy();
+    return res.status(200).send({
+      response: {
+        message: 'LEVEL_DELETED',
         code: 200,
       },
     });
